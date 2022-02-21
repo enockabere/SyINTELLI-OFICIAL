1af57c4e2d421669ab425429e27fcb3143eca981
enockabere <maebaenock95@gmail.com>
2022-02-22 00:28:44 +0300

    deployment

--- landing/views.py
+++ landing/views.py
@@ -8,6 +8,8 @@ def landing(request):
     ctx = {"all":all}
     return render(request,"index.html",ctx)
 def solution(request,pk):
+    topic = ALL_Category.objects.get(id=pk)
     offer = Offers.objects.filter(group=pk)
-    ctx = {"res":offer}
+    all = ALL_Category.objects.all()
+    ctx = {"res":offer,"topic":topic,"all":all}
     return render(request,"solution.html",ctx)
