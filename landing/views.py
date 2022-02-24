011ecd4538805b940d3f79aa597b7aa172afde30
enockabere <maebaenock95@gmail.com>
2022-02-24 10:57:40 +0300

    create senda mail functionality

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,10 @@
+from email import message
 from django.http import HttpResponse
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
 from . models import ALL_Category, Offers
+from django.core.mail import send_mail, BadHeaderError, EmailMessage
+from  django.conf import settings
+from django.contrib import messages
 
 # Create your views here.
 def landing(request):
@@ -11,5 +15,31 @@ def solution(request,pk):
     topic = ALL_Category.objects.get(id=pk)
     offer = Offers.objects.filter(group=pk)
     all = ALL_Category.objects.all()
-    ctx = {"res":offer,"topic":topic,"all":all}
+    emails = Offers.objects.all() 
+    name= ''
+    email = ""
+    service = ""
+    subject = ""
+    if request.method == "POST":
+        try:
+            name = request.POST.get('name')
+            email = request.POST.get('email')
+            service = request.POST.get('service')
+            subject = request.POST.get('subject')
+        except ValueError:
+            messages.error(request, "Not sent. Invalid Input, Try Again!!")
+            return redirect('solution', pk=id)
+    if name and email and service and subject:
+        try:
+            send_mail(
+                f"Quotation for {service}",
+                f"SY-INTELLI CONSULTING, \n\nTrust you are well, \n\nI'm requesting for a quotation on {service} to be sent to {email}. \n\nWith the following comments: {subject}.\n\nBest regards \n{name}.",
+                settings.DEFAULT_FROM_EMAIL,
+                ['sales@sy-intelli.com'],
+                fail_silently=False
+            )
+            print("True")
+        except BadHeaderError:
+            return redirect('solution', pk=id)    
+    ctx = {"res":offer,"topic":topic,"all":all,"emails":emails}
     return render(request,"solution.html",ctx)
