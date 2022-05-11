7889e102fcedea410a3a694b460d4c84f3875b81
enockabere <maebaenock95@gmail.com>
2022-05-11 08:29:56 +0300

    deploy changes

--- landing/models.py
+++ landing/models.py
@@ -32,7 +32,7 @@ class caseStudy(models.Model):
     Description = models.TextField()
     
     class  Meta: 
-        verbose_name_plural  =  "Case Study"
+        verbose_name_plural  =  "Differentiator"
     
     def __str__(self):
         return str(self.title)
